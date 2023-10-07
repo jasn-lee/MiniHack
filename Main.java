4081f52e1af0054ef8315eaadf99fc095f8da235
JjoshLee23 <joshuashingun1223@gmail.com>
2023-10-07 14:03:33 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,40 @@
 
+
 import java.util.Scanner;
+import java.time.LocalTime;
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
+        System.out.println("How many people are studying today? ");
+        int numberpeople=input.nextInt();
         System.out.println("How many minutes do you want to work for? ");
         int type=input.nextInt();
-        System.out.println("What is the current time");
-        String time=input.next();
-
-        String[] arroftime=time.split(":");
-        int[]arroftimeint=new int[2];
-        for (int i=0;i<=1;++i){
-            arroftimeint[i]=Integer.parseInt(arroftime[i]);
+        LocalTime currentTime = LocalTime.now();
+        System.out.println(currentTime); 
+        ArrayList<String>names=new ArrayList<String>();
+        ArrayList<Person> PersonList = new ArrayList<Person>();
+        for(int i=0;i<numberpeople;++i){
+            System.out.println("What is the name of the "+(i+1)+" person?");
+            String person=input.next();
+            names.add(person);
+            String yesname=names.get(i);
+            PersonList.add(new Person(yesname));
         }
-        int approxtimefinish=arroftimeint[1]+type;
+
+
+
+       
+       
+        
+
+;
+        
+        
+
+        
+
+        
         
         
         
