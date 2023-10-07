dc6197ea5d8583bae5733120e9d5aeff6a667b69
JjoshLee23 <joshuashingun1223@gmail.com>
2023-10-07 12:52:18 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,23 @@
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         System.out.println("How many minutes do you want to work for? ");
         int type=input.nextInt();
-         Calendar cal = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-        System.out.println( sdf.format(cal.getTime()) );
+        System.out.println("What is the current time");
+        String time=input.next();
+
+        String[] arroftime=time.split(":");
+        int[]arroftimeint=new int[2];
+        for (int i=0;i<=1;++i){
+            arroftimeint[i]=Integer.parseInt(arroftime[i]);
+        }
+        int approxtimefinish=arroftimeint[1]+type;
         
         
-       // Game game=new Game();
+        
     }
-}
+    }
+
+ 
