667ac1f4a1e705f30fb20edd49fe5fb3ce305fbf
JjoshLee23 <joshuashingun1223@gmail.com>
2023-10-07 11:56:17 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("How many minutes do you want to work for? ");
+        int type=input.nextInt();
+         Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        System.out.println( sdf.format(cal.getTime()) );
         
+        
+       // Game game=new Game();
     }
 }
